5feebc65943656220d6b30655fbf0df06399ce2b
alex-litinsky-auxi <126364433+alex-litinsky-auxi@users.noreply.github.com>
2023-03-01 22:05:34 +0000

    added text and title basis

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ promptAgenda = prompt + "Given this context, generate an agenda outline for my p
 titlePres = openai.Completion.create(prompt=promptPres, model='text-davinci-003', temperature=0.5, max_tokens=100)['choices'][0]['text']
 execSummary = openai.Completion.create(prompt=promptExecSummary, model='text-davinci-003', temperature=0.5, max_tokens=100)['choices'][0]['text']
 agenda = openai.Completion.create(prompt=promptAgenda, model='text-davinci-003', temperature=0.5, max_tokens=100)['choices'][0]['text']
-    
+  
     # with the provided text, we can now create a slides with the title presentation, executive summary, and agenda
 
 
@@ -43,12 +43,16 @@ print(execSummary)
 print(agenda)
 userSatisfaction = input("\n User, are you satisfied with the initial template? Y/N")
 
-if userSatisfaction == "Y":
+if userSatisfaction.lower() == "y":
     SLD_LAYOUT_TITLE_AND_CONTENT = 1
 
     prs = Presentation()
     slide_layout = prs.slide_layouts[SLD_LAYOUT_TITLE_AND_CONTENT]
     slide = prs.slides.add_slide(slide_layout)
+
+    title_placeholder = slide.shapes.title
+    title_placeholder.text = 'Agenda Example'
+    slide.add_paragraph("Whatever you want to say here.")
 
 
 
