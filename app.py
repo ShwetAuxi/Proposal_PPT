1fdab423be4b2ca0b222b971e9ff901fc044a624
alex-litinsky-auxi <126364433+alex-litinsky-auxi@users.noreply.github.com>
2023-03-01 21:48:56 +0000

    one more new change

--- app.py
+++ app.py
@@ -42,4 +42,8 @@ SLD_LAYOUT_TITLE_AND_CONTENT = 1
 prs = Presentation()
 slide_layout = prs.slide_layouts[SLD_LAYOUT_TITLE_AND_CONTENT]
 slide = prs.slides.add_slide(slide_layout)
-slide.a
+
+
+
+
+prs.save("userui.pptx")
